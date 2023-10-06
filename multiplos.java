d0106da8bd7477043874c5912e195905b3ee191a
jenanda123 <143815657+jenanda123@users.noreply.github.com>
2023-10-06 08:07:06 -0300

    Add files via upload

---
+++ multiplos.java
@@ -0,0 +1,13 @@
+
+public class multiplos {
+	public static void main(String[] args) {
+		for (int i = 1; i < 101; i++) {
+			if (i%10 == 0) {
+				System.out.print(i+" é multiplo de 10\n");	
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
